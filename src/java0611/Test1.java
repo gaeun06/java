ee91e76792893b9f2d70036f6169fa40937b81e4
gaeun06 <chlrkdms55@naver.com>
2025-06-11 17:12:30 +0900

    Update Test1.java

--- src/java0611/Test1.java
+++ src/java0611/Test1.java
@@ -1,8 +1,12 @@
-package java0611;
+package Java0611;
 
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.print("a");
+        System.out.print("b");
+        System.out.println("c");
+        System.out.print("d");
     }
 }
