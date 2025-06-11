74d37d9c0e103240fdea0310ff0cbb9e38359b60
gaeun06 <chlrkdms55@naver.com>
2025-06-11 22:03:41 +0900

    fix

---
+++ src/java0611/Test1.java
@@ -0,0 +1,10 @@
+package java0611;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("a");
+        System.out.println("bc");
+        System.out.println("test");
+    }
+}
