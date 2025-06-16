90c9c5446db095c8c9eed39a7a1d207223977521
gaeun06 <chlrkdms55@naver.com>
2025-06-16 17:12:33 +0900

    0616

---
+++ src/a1_type/LifeCycle.java
@@ -0,0 +1,21 @@
+package a1_type;
+
+public class LifeCycle {
+    int value = 0;
+    public static void main(String[] args) {
+        // 변수의 생명 주기
+        // 중괄호{}로 감싸진 블록 범위에 따라 결정된다.
+        /*
+        1. 지역 변수 : 메서드, if문, for문 등의 블록 내부
+        - 해당 블록이 실행될 때 생성되고 블록 종료시 즉시 소멸
+        */
+        int x = 100;
+        if(true) {
+            { int y = 50; }
+            int y = 40;
+        }
+        int y = 60;
+        System.out.println(x); // 100
+        System.out.println(y); // 60
+    }
+}
