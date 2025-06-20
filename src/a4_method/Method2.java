58e1d7f0019bd50ea9ac995fc8e43aa2430dc833
gaeun06 <chlrkdms55@naver.com>
2025-06-20 12:50:31 +0900

    0620

--- src/a4_method/Method2.java
+++ src/a4_method/Method2.java
@@ -9,12 +9,12 @@ public class Method2 {
         int a = 3;
         int b = 5;
         int c = 10;
-        int value = plus(3, 5); // 함수의 계산 결과를 변수에 저장함
+        int value = plus(a, b); // 함수의 계산 결과를 변수에 저장함, 리턴형 함수의 형태-int
         multiply(value, c); // 저장한 계산 결과를 다른 함수가 이용할 수 있음
     }
 
-    public static int plus (int a, int b) {
-        int result = a + b;
+    public static int plus (int a, int b) { // 실행 코드가 아님. 코드의 선언 구호(메서드)
+        int result = a + b; // 코드의 실행 구호는 여기 부터!
         return result;
     }
     public static void multiply(int x, int y) {
