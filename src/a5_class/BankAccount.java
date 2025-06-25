19bfb89cfbf35db4154b0c2b19f2bbf94eed58d0
gaeun06 <chlrkdms55@naver.com>
2025-06-25 14:36:45 +0900

    0625

---
+++ src/a5_class/BankAccount.java
@@ -0,0 +1,16 @@
+package a5_class;
+
+public class BankAccount {
+    // 필드
+    // accountNumber 계좌번호
+    // ownerName 예금주 이름
+    // balance 잔액
+    // 생성자
+    // 메서드
+    // deposit(입금할 금액) : 돈을 입금하는 메서드 (양수만 가능)
+    // withdraw(출금할 금액) : 돈을 출금하는 메서드
+    //                      (양수만 가능, 잔액보다 출금액이 많으면 경고 표시)
+    // toString() : 계좌번호, 예금주 이름, 잔액 화면 출력
+
+
+}
