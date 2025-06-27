f96b8731d6ad11c96b391c4651ee177ed5076d13
gaeun06 <chlrkdms55@naver.com>
2025-06-27 17:30:11 +0900

    0627

--- src/a6_array/Example5.java
+++ src/a6_array/Example5.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Example5 {
     public static void main(String[] args) {
-        // 사용자로부터 이름을 입력받아 아래 회원배열에 있는 이름이면
+        // 사용자로부터 이름을 입력받아 아래 회원 배열에 있는 이름이면
         // "~~~님 환영합니다"를 출력하고
-        // 없으면 "회원가입해주세요"를 출력하는 메서드 (단, 대소문자 구분하지 마시오)
+        // 없으면 "회원가입을 해주세요"를 출력하는 메서드 (단, 대소문자 구분하지 마시오)
         String[] members = {"Steve", "Tom", "Michael",
                 "Laura", "Jessica", "Annie"};
         Scanner scanner = new Scanner(System.in);
@@ -15,6 +15,11 @@ public class Example5 {
         System.out.println(checkMember(members, str));
     }
     public static String checkMember(String[] members, String str) {
-        return "";
+        for (String data : members) {
+            if (data.equalsIgnoreCase(str)) {
+                System.out.println(members + "님 환영합니다.");
+            }
+        }
+        return str;
     }
 }
